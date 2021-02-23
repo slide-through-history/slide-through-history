0a969ce2595cbd29f84348b2403898d64c307101
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-02-23 11:50:30 -0500

    Create Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link } from "react-router-dom";
+
+
+function Footer() {
+  return (
+    <div className="px-5 py-3 bg-purple-300 mb-0">
+      <p className="text-center">Copyright 2021</p>
+    </div>
+    );
+  
+}
+
+export default Footer;
