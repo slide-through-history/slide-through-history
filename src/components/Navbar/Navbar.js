8b3ce2889ad2a1175dc57364dfab9d072cb993bf
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-02-23 11:54:53 -0500

    Update navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-purple-500 mb-3">
+        <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-purple-400 mb-3">
   <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
     <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
       <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="#pablo">
