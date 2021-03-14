33d98b6fc447b57a7aa9ff7fda597ab534c7f067
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2021-03-13 21:11:06 -0500

    Add Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,16 @@ import { BrowserRouter, Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="px-5 py-3 bg-purple-300 mb-0">
-      <p className="text-center">Copyright 2021</p>
+      <p className="text-center">Copyright 2021 <i class="fab fa-linkedin"></i>
+
+<i class="fab fa-linkedin"></i>
+
+<i class="fab fa-linkedin"></i>
+
+<i class="fab fa-linkedin"></i></p>
+      
+
+
     </div>
     );
   
