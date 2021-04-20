ce33cf71d9866204704f022a4f6199223f74bc4c
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-04-19 20:54:31 -0400

    fixed another merge conflict app.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,6 @@ const App = () => {
         deaths={Deaths}
         events={Events}
       />
-<<<<<<< HEAD
       <Router>
         <div>
           <Switch>
@@ -97,19 +96,6 @@ const App = () => {
           </Switch>
         </div>
       </Router>
-=======
-      <div>
-        <Router>
-          <div>
-            <Switch>
-              {/* <Route exact path="/" component={Home} /> */}
-              <Route exact path="/login" component={Login} />
-              {/* <Route exact path="/signup" component={Signup} /> */}
-            </Switch>
-          </div>
-        </Router>
-      </div>
->>>>>>> origin/staging
     </div>
   );
 };
